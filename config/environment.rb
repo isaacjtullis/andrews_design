ed642a97fa68928c86640b1d494271b9584bad30
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-10 12:44:46 -0700

    added website to .env file

--- config/environment.rb
+++ config/environment.rb
@@ -8,9 +8,9 @@ ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SEND_GRID_USERNAME"],
   :password => ENV["SEND_GRID_PASSWORD"],
   :key => ENV["SEND_GRID_KEY"],
-  :domain => ENV["andrews-design.herokuapp.com"],
+  :domain => ENV["HEROKU_WEBSITE"],
   :address => 'smtp.sendgrid.net',
   :port => 587,
-  :authentication => :login,
+  :authentication => 'login',
   :enable_starttls_auto => true
 }
