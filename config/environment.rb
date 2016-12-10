f527626abfffb999e3bfb1eb68e77575b66de227
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-10 12:28:40 -0700

    Changed where I added ActionMailer information to config/environment.rb - config/environments/environment.rb

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,14 @@ require_relative 'application'
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  :user_name => ENV["SEND_GRID_USERNAME"],
+  :password => ENV["SEND_GRID_PASSWORD"],
+  #:key => ENV["SEND_GRID_KEY"],
+  :domain => ENV["andrews-design.herokuapp.com"],
+  :address => 'smtp.sendgrid.net',
+  :port => 587,
+  :authentication => :plain,
+  :enable_starttls_auto => true
+}
