eac26481adf3e96b2a26440101b542406d32f5b4
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-10 13:21:23 -0700

    added changes to environment.rb file

--- config/environment.rb
+++ config/environment.rb
@@ -4,13 +4,13 @@ require_relative 'application'
 # Initialize the Rails application.
 Rails.application.initialize!
 
+ActionMailer::Base.delivery_method = :smtp
 ActionMailer::Base.smtp_settings = {
   :user_name => ENV["SEND_GRID_USERNAME"],
   :password => ENV["SEND_GRID_PASSWORD"],
-  :key => ENV["SEND_GRID_KEY"],
   :domain => ENV["HEROKU_WEBSITE"],
   :address => 'smtp.sendgrid.net',
   :port => 587,
-  :authentication => 'login',
+  :authentication => :plain,
   :enable_starttls_auto => true
 }
