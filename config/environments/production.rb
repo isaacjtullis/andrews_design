bb1b9a8f7404242ce9448181634b119a96fb09e3
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-11 22:55:37 -0700

    Fixed production environment to deploy e-mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,17 +2,14 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :smtp
-  #config.action_mailer.default_url_options = { :host => ENV["HEROKU_WEBSITE"] }
 
   ActionMailer::Base.smtp_settings = {
     :address => 'smtp.sendgrid.net',
     :port => 587,
     :authentication => :plain,
-    #:key => ENV["SEND_GRID_KEY"]
     :user_name => ENV["SENDGRID_USERNAME"],
     :password => ENV["SENDGRID_PASSWORD"],
     :domain => ["heroku.com"]
-    #:enable_starttls_auto => true
   }
   config.action_mailer.default_url_options = { :host => 'http://andrews-design.herokuapp.com' }
   # Code is not reloaded between requests.
