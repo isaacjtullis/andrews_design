77d42632ba5edc8c6d8913dc1af10bc49d28647f
Isaac Tullis <isaacjtullis@gmail.com>
2016-12-12 17:06:49 -0700

    Changed Tests

--- spec/features/user_sends_an_email_spec.rb
+++ spec/features/user_sends_an_email_spec.rb
@@ -1,13 +1,18 @@
 require 'rails_helper'
-
+require 'pry'
 feature 'User sends their information', %Q{
   As an visitor,
   I want to contact the people at Andrews Design,
   So that I can hire them for projects.
 } do
-
+  before do
+    Admin.create(id: 1, first_name: 'Isaac', last_name: 'Tullis', bio: 'HEY!')
+    Admin.create(id: 2, first_name: 'Isaac', last_name: 'Tullis', bio: 'HEY!')
+    Admin.create(id: 3, first_name: 'Mary', last_name: 'Tullis', bio: 'HEY!')
+  end
   scenario 'user visits home page' do
     visit root_path
+    find('.contact-button').click
 
     fill_in 'user_first_name', with: 'John'
     fill_in 'user_last_name', with: 'Smith'
@@ -18,6 +23,7 @@ feature 'User sends their information', %Q{
   end
   scenario "Create a contact and deliver mail" do
     visit root_path
+    find('.contact-button').click
 
     fill_in 'user_first_name', with: 'John'
     fill_in 'user_last_name', with: 'Smith'
@@ -29,6 +35,7 @@ feature 'User sends their information', %Q{
 
   scenario 'Contact not created correctly no mail delievered' do
     visit root_path
+    find('.contact-button').click
 
     fill_in 'user_first_name', with: ''
     fill_in 'user_last_name', with: 'Smith'
